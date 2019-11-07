353902be6ccc2c2d92ccbfd7fcfa2530c5a4af18
Jamal Frank <jamaljfrank@gmail.com>
2019-11-07 02:15:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "HUH?! SPEAK UP, SONNY!"
-  else phrase == phrase.upcase
+  elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else !phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
